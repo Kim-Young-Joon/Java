0e885e49c365c08fc20b4f032cad07e1628ea726
Kim-Young-Joon <yj3723@naver.com>
2022-07-12 22:02:54 +0900

    study 0712 - inheritance

---
+++ SampleProject/src/com/uni/Student.java
@@ -0,0 +1,39 @@
+package com.uni;
+
+public class Student extends Human {
+	// fields
+	private String number;
+	private String major;
+	
+	// constructors
+	public Student() {
+		
+	}
+	
+	public Student(String name, int age, int height, int weight, String number, String major) {
+		super(name, age, height, weight);
+		this.number = number;
+		this.major = major;
+	}
+	
+	// methods
+	public String printInformation() {
+		return super.printInformation() + " " + this.getNumber() + " " + this.getMajor();
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getMajor() {
+		return major;
+	}
+
+	public void setMajor(String major) {
+		this.major = major;
+	}
+}
